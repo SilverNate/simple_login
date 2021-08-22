504a9be99bc8d4c54db8a81e14324486d3d707ef
andean <andean@thefthing.com>
2021-08-22 08:42:45 +0700

    update

---
+++ src/services/auth.service.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:8000/api/';
+
+class AuthService {
+  login(user) {
+    return axios
+      .post(API_URL + 'auth/login', {
+        email: user.email,
+        password: user.password
+      })
+      .then(response => {
+        console.log('token', response.data)
+        if (response.data.access_token) {
+          localStorage.setItem('user', JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+
+  register(user) {
+    return axios.post(API_URL + 'auth/registerAdmin', {
+      school_name: user.school_name,
+      email: user.email,
+      password: user.password
+    });
+  }
+}
+
+export default new AuthService();
